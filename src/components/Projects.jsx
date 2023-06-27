e5a8be2bc85e3470c4b5ae1658d6604b5912b134
oladee <deemajor230600@gmail.com>
2023-06-27 13:43:43 +0100

    included card design for project display

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import image from '../assets/image.png'
 
 const Projects = () => {
   return (
@@ -9,7 +10,26 @@ const Projects = () => {
         </header>
         <div className="cardSection">
           <div className="card">
-            
+            <div className="leftpane">
+              <h3>Project Name</h3>
+              <p>I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.</p>
+              <button>View project</button>
+            </div>
+            <div className="rightpane">
+              <img src={image} alt="" width='300'/>
+            </div>
+          </div>
+        </div>
+        <div className="cardSection">
+          <div className="card">
+            <div className="leftpane">
+              <h3>Project Name</h3>
+              <p>I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.</p>
+              <button>View project</button>
+            </div>
+            <div className="rightpane">
+              <img src={image} alt="" width='300'/>
+            </div>
           </div>
         </div>
       
