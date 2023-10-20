a0c257a9a3856deec68e94c7241b9b70c45a8f62
oladee <deemajor230600@gmail.com>
2023-10-20 01:40:48 +0100

    adjusted project images

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 export const data = [
     {
+        name: 'Image Gallery App',
+        description: 'This is a image gallery where users can interact through drag and drop functionalities and also search through the library for different queries of images. Image result is limited to 8',
+        url: 'https://image-dnd.netlify.app/',
+        image: '/assets/imageGalleryLogin.PNG'
+    },
+    {
         name: 'MovieBox',
         description: 'This is a movie site where users can interact by searching for movies, adding favourites and also get details about the movie by clicking on the associated cards.',
         url: 'https://hngxstage2.netlify.app/',
-        image: '/assets/Rec1.svg'
+        image: '/assets/moviebox.PNG'
     },
     {
         name: 'MetaBnB',
         description: 'I created this personal project in order to show how to create an interface in Figma using a portfolio as an example.',
         url: 'https://oladee.github.io/MetaBnB/',
-        image: '/assets/Rec2.svg'
-    },
-    {
-        name: 'Image Gallery App',
-        description: 'This is a image gallery where users can interact by through drag and drop functionalities and also search through the library for different queries of images. Image result is limited to 8',
-        url: 'https://image-dnd.netlify.app/',
-        image: '/assets/Rec3.svg'
+        image: '/assets/metabnb.PNG'
     },
 
 ]
